03ad7cbd19b8dc14cb6ca3be7757442009b70e6b
Matt Johnson <mjohnson@sonatype.com>
2026-04-10 07:45:56 -0400

    feat: add Spring Boot application entry point and Todo entity

---
+++ src/main/java/com/example/todo/Todo.java
@@ -0,0 +1,33 @@
+package com.example.todo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private boolean completed;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
